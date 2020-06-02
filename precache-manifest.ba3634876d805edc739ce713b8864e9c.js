3955a37de8737571af08c9075358d0da32a253ae
Kate <katerina_ti@mail.ru>
2020-06-02 17:21:45 +0300

    Updates

---
+++ precache-manifest.ba3634876d805edc739ce713b8864e9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80eed861ccac49d46553d252617bc044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8343fd76447df99bda36",
+    "url": "/static/css/main.4976d729.chunk.css"
+  },
+  {
+    "revision": "85c4c08fe316ff922959",
+    "url": "/static/js/2.053201d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.053201d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8343fd76447df99bda36",
+    "url": "/static/js/main.883350af.chunk.js"
+  },
+  {
+    "revision": "dce78132577e590565b0",
+    "url": "/static/js/runtime-main.31e3810f.js"
+  }
+]);
